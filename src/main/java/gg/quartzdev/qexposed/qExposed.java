665918ae989fdfbf8cdae5d0092bc3b194e072e5
QarthO <qartho@gmail.com>
2024-01-06 13:55:00 -0500

    initial project structure

---
+++ src/main/java/gg/quartzdev/qexposed/qExposed.java
@@ -0,0 +1,17 @@
+package gg.quartzdev.qexposed;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class qExposed extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
